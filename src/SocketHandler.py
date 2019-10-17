6891bb60e05ab69368c5660371728613a38a250a
Chad <ChadAndrewWolfe@gmail.com>
2019-10-17 11:38:49 -0400

    Formatted Window and created skeleton for SocketHandler

---
+++ src/SocketHandler.py
@@ -0,0 +1,51 @@
+
+class SocketHandler:
+    """
+    This class serves as a Socket wrapper for easy socket manipulation of the program.
+    """
+
+    def __init__(self):
+        pass
+
+    def establish_connection(self) -> bool:
+        """
+        Establishes a connection to a peer.
+        """
+        pass
+
+    def close_connection(self) -> None:
+        """
+        Closes the connection with a peer.
+        """
+        pass
+
+    def open_as_host(self) -> bool:
+        """
+        Opens connections with this machine as host.
+        """
+        pass
+
+    def close_as_host(self) -> None:
+        """
+        Closes all connections with this machine as host.
+        :return:
+        """
+        pass
+
+    def add_network_action_handler(self) -> bool:
+        """
+        Adds a NetworkActionHandler to this SocketHandler.
+        """
+        pass
+
+    def remove_network_action_handler(self) -> bool:
+        """
+        Removes the specified NetworkActionHandler from this SocketHandler.
+        """
+        pass
+
+    def send_packet(self) -> bool:
+        """
+        Broadcasts the specified packet to all peers.
+        """
+        pass
--- src/Window.py
+++ src/Window.py
@@ -3,6 +3,10 @@ from tkinter import filedialog
 
 
 class Window:
+    """
+    This class handles all display aspects of Jum.py.
+    """
+
     root = Tk()
     text = Text(root) 
     currentFile = None
@@ -16,27 +20,36 @@ class Window:
         self.show()
 
     def create(self) -> None:
+        """
+        Creates the window.
+        """
         self.root.title("Untitled")
-        self.fileMenu.add_command(label = "open", command = self.openFile)
+        self.fileMenu.add_command(label="open", command = self.openFile)
         self.text.pack()
 
-
     def show(self) -> None:
+        """
+        Shows the window.
+        """
         self.text.mainloop()
 
-    def openFile(self):
+    def open_file(self) -> None:
+        """
+        Prompts the user to open a file.
+        """
         f = filedialog.askopenfilename(defaultextension=".txt",)
         print(f)
         if f is None or f == "":
             self.currentFile = None
         else:
             self.currentFile = f
-            self.text.delete(1.0,END) 
-            f = open(self.currentFile,"r") 
-            self.text.insert(1.0,f.read()) 
+            self.text.delete(1.0, END)
+            f = open(self.currentFile, "r")
+            self.text.insert(1.0, f.read())
             f.close()
+
 
 if __name__ == "__main__":
     w = Window()
-    #w.openFile()
-    #w.show()
+    # w.open_file()
+    # w.show()
