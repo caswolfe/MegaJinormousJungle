3e31c501d77b334b70ac4d1721d8dcfa69c0fff1
Andrew Duffield <duffy518@gmail.com>
2019-10-27 14:01:38 -0400

    New Cursor file

---
+++ src/DataPacketCursorUpdate.py
@@ -0,0 +1,63 @@
+import difflib
+import hashlib
+from enum import Enum
+import logging
+import json
+
+try:
+    from src.DataPacket import DataPacket
+except ImportError as ie:
+    try:
+        # TODO: linux imports
+        from DataPacket import DataPacket
+    except ImportError as ie2:
+        print('cant import???')
+        exit(-1)
+
+
+class DataPacketCursorUpdate(DataPacket):
+
+    def __init__(self, old_text: str = None, new_text: str = None, json_str: str = None):
+        super().__init__()
+        self.log = logging.getLogger('jumpy')
+
+        # default values
+        self.action = Action.NONE
+        self.position = -1
+        self.character = ''
+        self.data_dict.update({'new_text': new_text})
+
+        if json_str is None:
+            self.old_text_hash = hashlib.sha1(old_text.encode()).hexdigest()
+            self.log.debug('{} => {}'.format(repr(old_text), repr(new_text)))
+            for i, s in enumerate(difflib.ndiff(old_text, new_text)):
+                if s[0] == ' ':
+                    # self.action = Action.NONE
+                    # self.position = -1
+                    # self.character = ''
+                    # self.log.debug('None')
+                    pass
+                elif s[0] == '-':
+                    self.action = Action.REMOVE
+                    self.position = i
+                    self.character = s[-1]
+                    self.log.debug('Delete \'{}\' from position \'{}\''.format(self.character, self.position))
+                elif s[0] == '+':
+                    self.action = Action.ADD
+                    self.position = i
+                    self.character = s[-1]
+                    self.log.debug('Add \'{}\' to position \'{}\''.format(self.character, self.position))
+            self.data_dict.update({'action': self.action.value})
+            self.data_dict.update({'position': self.position})
+            self.data_dict.update({'character': self.character})
+        else:
+            self.data_dict = json.loads(json_str)
+
+    def __str__(self) -> str:
+        return '{} - {} - {} - {}'.format(self.old_text_hash, self.action, self.position, self.character)
+
+
+class Action(Enum):
+    NONE = 0
+    ADD = 1
+    REMOVE = 2
