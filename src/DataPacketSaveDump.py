b7866cea5d851714f56c7daea44976dfe94bff3b
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2019-11-17 18:23:52 -0500

    Added document specification for Save Request & Dump

--- src/DataPacketSaveDump.py
+++ src/DataPacketSaveDump.py
@@ -17,14 +17,18 @@ class DataPacketSaveDump(DataPacket):
         super().__init__()
         self.log = logging.getLogger('jumpy')
 
+        self.document: str = None
         self.text_hash: str = None
         self.text: str = None
 
-    def define_manually(self, text):
+    def define_manually(self, document: str, text: str):
+        # TODO: discuss if we should compress the text
+        self.document = document
         self.text_hash = DataPacketDocumentEdit.get_text_hash(text)
         self.text = text
         self.update_data_dict()
 
     def update_data_dict(self) -> None:
+        self.data_dict.update({'document': self.text_hash})
         self.data_dict.update({'text_hash': self.text_hash})
         self.data_dict.update({'text': self.text})
--- src/DataPacketSaveRequest.py
+++ src/DataPacketSaveRequest.py
@@ -16,3 +16,12 @@ class DataPacketSaveRequest(DataPacket):
     def __init__(self):
         super().__init__()
         self.log = logging.getLogger('jumpy')
+
+        self.document: str = None
+
+    def define_manually(self, document: str):
+        self.document = document
+        self.update_data_dict()
+
+    def update_data_dict(self) -> None:
+        self.data_dict.update({'document': self.document})
