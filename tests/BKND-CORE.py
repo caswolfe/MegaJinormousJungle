979469aab2640c53de35dd61aca0652a82f9b55e
Ben <pierce@case.edu>
2019-11-20 03:16:20 -0500

    tests

--- tests/BKND-CORE.py
+++ tests/BKND-CORE.py
@@ -16,7 +16,8 @@ class MyTestCase(unittest.TestCase):
         assert len(syn.COLORS) >= len(syn.keyword_list)
         assert syn.assign_colors()
         assert len(syn.get_keywords()) > 0
-        assert len(syn.get_color_dict()) > 0    
+        assert len(syn.get_color_dict()) > 0
+    
 
 
 
--- tests/TestingManager.py
+++ tests/TestingManager.py
@@ -1,43 +1,5 @@
-from DataPacket import DataPacket
-from DataPacketDocumentEdit import Action
-from Window import Window
-from NetworkActionHandler import NetworkActionHandler
-from NetworkHandler import NetworkHandler
 import unittest
-
-
-class testConnection(unittest.TestCase):
-    def setup(self):
-        pass
-
-    """
-    Test creation of window
-    """
-    def testA(self):
-        self.window = Window.Window()
-        assert self.window
-
-    """
-    Test creation of network handelers
-    """
-    def testB(self):
-        self.net_hand = NetworkHandler()
-        self.nah = NetworkActionHandler(self)
-        self.net_hand.add_network_action_handler(self.nah)
-        assert self.net_hand
-        assert self.nah
-    """
-    Test connection itself
-    """
-    def testC(self):
-        self.net_hand = NetworkHandler()
-        self.nah = NetworkActionHandler(self)
-        self.net_hand.add_network_action_handler(self.nah)
-        self.net_hand.establish_connection()
-        assert self.net_hand.is_connected
-        self.net_hand.close_connection()
-        assert not self.net_hand.is_connected
-        
-
+from NWK_CORE import MyTestCase
+from BKND_CORE import  MyTestCase as t2
 if __name__ == "__main__":
     unittest.main()
