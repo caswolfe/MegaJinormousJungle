b9508df760860f5ce393954705c359cc8d7d9472
Ben <pierce@case.edu>
2019-11-20 02:54:15 -0500

    request join tests

--- tests/NWK-CORE.py
+++ tests/NWK-CORE.py
@@ -3,9 +3,10 @@ import unittest
 import logging
 import sys
 
-from DataPacket import DataPacket
+
 
 sys.path.append("../src/")
+from DataPacket import DataPacket
 
 
 class Helper:
@@ -191,7 +192,56 @@ class MyTestCase(unittest.TestCase):
         # packets: list[DataPacketDocumentEdit] = DataPacketDocumentEdit.generate_packets_from_changes(old_text, new_text, 'test_doc')
         # applied_text = DataPacketDocumentEdit.apply_multiple_packets(old_text, packets)
         # assert applied_text == new_text
+    @staticmethod
+    def test_NWK_14():
+        """
+        Tests a DataPacketRequestJoin
+        """
+    
+        # basic logging init
+        log = logging.getLogger('jumpy')
+        log_format = logging.Formatter('%(filename)s - %(lineno)d - %(levelname)s - %(message)s')
+        log.setLevel(logging.DEBUG)
+    
+        # logging console init
+        log_handler_console = logging.StreamHandler()
+        log_handler_console.setLevel(logging.DEBUG)
+        log_handler_console.setFormatter(log_format)
+        log.addHandler(log_handler_console)
+    
+        # imports needed for this test
+        from DataPacketRequestJoin import DataPacketRequestJoin
+        from NetworkHandler import NetworkHandler
+        import time
+        helper = Helper()
 
+        # base setup
+        net_hand = NetworkHandler(helper.parse_message)
+        net_hand.establish_connection()
+    
+        time.sleep(1)
+    
+        assert net_hand.is_connected
+    
+        packet: DataPacketRequestJoin = DataPacketRequestJoin()
+        assert packet.data_dict.keys().__contains__('packet-name')
+        assert packet.data_dict.keys().__contains__('mac-addr')
+        assert packet.data_dict.keys().__contains__('time-of-creation')
+        assert not packet.data_dict.keys().__contains__('time-of-send')
+    
+        assert packet.data_dict.get('packet-name').__eq__('DataPacketRequestJoin')
+        assert packet.data_dict.get('mac-addr') is not None
+        assert packet.data_dict.get('time-of-creation') is not None
+    
+        packet.set_time_of_send()
+        assert packet.data_dict.keys().__contains__('time-of-send')
+        assert packet.data_dict.get('time-of-send') is not None
+    
+        net_hand.send_packet(packet)
+        time.sleep(1)
+        net_hand.close_connection()
+        assert helper.packet_received_data_dict is not None
+        pass
     # # @staticmethod
     # # def test_NWK_14():
     # #     """
