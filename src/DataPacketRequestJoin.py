d30181e7662d85d26f6af0665212854e6ed1c7c8
Ben <pierce@case.edu>
2019-11-20 00:43:43 -0500

    fixed join import bug

--- src/DataPacketRequestJoin.py
+++ src/DataPacketRequestJoin.py
@@ -1,15 +1,6 @@
 import logging
-
-try:
-    from src.DataPacket import DataPacket, DataPacketDocumentEdit
-except ImportError as ie:
-    try:
-        # TODO: linux imports
-        from DataPacket import DataPacket, DataPacketDocumentEdit
-    except ImportError as ie2:
-        print('cant import???')
-        exit(-1)
-
+from DataPacket import DataPacket
+from DataPacketDocumentEdit import DataPacketDocumentEdit
 
 class DataPacketRequestJoin(DataPacket):
 
