d1fe46f21024456580860ac195bf1b5b476bac43
Ben <pierce@case.edu>
2019-11-24 18:49:00 -0500

    fix workspace

--- jumpy_temp/DataPacketRequestJoin.py
+++
@@ -1,9 +0,0 @@
-import logging
-from DataPacket import DataPacket
-from DataPacketDocumentEdit import DataPacketDocumentEdit
-
-class DataPacketRequestJoin(DataPacket):
-
-    def __init__(self):
-        super().__init__()
-        self.log = logging.getLogger('jumpy')
--- jumpy_temp/jumpy_temp/TestingManager.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from NWK_BASE import MyTestCase as t1
-from NWK_CORE import MyTestCase as t2
-from BKND_CORE import  MyTestCase as t3
-
-
-if __name__ == "__main__":
-    unittest.main()
